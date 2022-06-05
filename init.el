7674a45b37b5b4b756c08ff1d581f1889dfb8d20
Michael Winsor <mgwinsor@gmail.com>
2022-06-05 12:20:04 +0800

    feat: configure straight pkg manager, install helm

--- init.el
+++ init.el
@@ -5,23 +5,39 @@
 
 
 ;; Install use-package if necessary
-(require 'package)
-(setq package-archives (append package-archives
-			       '(("melpa" . "http://melpa.org/packages/"))))
-(package-initialize)
+;;(require 'package)
+;;(setq package-archives (append package-archives
+;;			       '(("melpa" . "http://melpa.org/packages/"))))
+;;(package-initialize)
 
 ;; Bootstrap 'use-package'
-(unless (package-installed-p 'usepackage)
-  (package-refresh-contents)
-  (package-install 'use-package))
-
-(use-package diminish :ensure t)
+;;(unless (package-installed-p 'usepackage)
+;;  (package-refresh-contents)
+;;  (package-install 'use-package))
+;;
+;;(use-package diminish :ensure t)
 
 ;; Enable use-package
-(eval-when-compile
-  (require 'use-package))
-(require 'diminish)
-(require 'bind-key)
+;;(eval-when-compile
+;;  (require 'use-package))
+;;(require 'diminish)
+;;(require 'bind-key)
+
+
+;; == Straight Package Manager ==
+(defvar bootstrap-version)
+(let ((bootstrap-file
+       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+      (bootstrap-version 5))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+        (url-retrieve-synchronously
+         "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
+         'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
+
 
 
 ;; == UI Tweaks ==
@@ -40,13 +56,18 @@
 
 ;; == Evil Mode ==
 (use-package evil
-  :ensure t
-  :diminish undo-tree-mode
+  :straight t
+  ;;:diminish undo-tree-mode
   :init (setq evil-want-C-i-jump nil)
   :config (evil-mode 1)
 
   ;; Resolve sentence jumping issue
   (setq sentence-end-double-space nil))
+
+
+;; == Helm ==
+(use-package helm
+  :straight t)
 
 
 ;; == Org Mode ==
@@ -78,16 +99,12 @@
 
 ;; ==Custom set variables==
 (custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
  '(org-capture-templates
    '(("b" "Book" entry
       (file "read.org")
       (file "/Users/mw/org/templates/book.org"))))
- '(org-enforce-todo-dependencies t)
- '(package-selected-packages '(evil diminish use-package)))
+ '(org-enforce-todo-dependencies t))
+ ;;'(package-selected-packages '(evil diminish use-package)))
 
 
 ;; ==Key mappings==
